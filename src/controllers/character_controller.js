54cc920b5ba94e95810101035d916bd7fb9f0295
fztakeshi <tfmanaka@uc.cl>
2022-07-07 06:56:57 -0400

    :sparkles: First endpoint (request N characters)

--- src/controllers/character_controller.js
+++ src/controllers/character_controller.js
@@ -1,11 +1,40 @@
-import models from '../models'
+// import models from '../models'
 import BaseController from './base'
+
+const axios = require('axios').default
 
 export default class CharacterController extends BaseController {
   CharacterController () { }
 
   async index (req, res) {
-    return super.Success(res, '')
+    try {
+      const id = parseInt(req.params.id)
+      const arr = await [...Array(id).keys()].map(idx => idx + 1)
+      const { data } = await axios({
+        method: 'get',
+        url: `https://rickandmortyapi.com/api/character/${arr.toString()}`
+      })
+      if (id > 1) {
+        data.forEach((character) => {
+          arr[character.id - 1] = {
+            name: character.name,
+            status: character.status,
+            species: character.species,
+            origin: character.origin.name
+          }
+        })
+      } else {
+        arr[data.id - 1] = {
+          name: data.name,
+          status: data.status,
+          species: data.species,
+          origin: data.origin.name
+        }
+      }
+      return super.Success(res, arr)
+    } catch (err) {
+      /* TODO: Manejar errores? */
+    }
   }
 
   async create (req, res) {
--- src/routes/character_routes.js
+++ src/routes/character_routes.js
@@ -1,8 +1,11 @@
 import app from 'express'
-import asyncHandler from 'express-async-handler'
+
+// import asyncHandler from 'express-async-handler'
 
 import CharacterController from '../controllers/character_controller'
 
 const routes = app.Router()
+
+routes.get('/:id', new CharacterController().index)
 
 export default routes
