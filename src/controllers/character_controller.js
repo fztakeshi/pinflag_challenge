2f45bf1349c8066b6356e9009e9ef6062e75a322
Takeshi Manaka <tfmanaka@uc.cl>
2022-12-05 02:44:08 -0300

    feat: add first endpoint to find  N characters

--- src/controllers/character_controller.js
+++ src/controllers/character_controller.js
@@ -1,11 +1,15 @@
-import models from '../models'
+/* import models from '../models' */
 import BaseController from './base'
+import { getCharacters } from '../services/character_service'
 
 export default class CharacterController extends BaseController {
   CharacterController () { }
 
   async index (req, res) {
-    return super.Success(res, '')
+    const { number } = req.params
+    const arrayNumber = Array.from({ length: number }, (_, i) => i + 1)
+    const characters = await getCharacters(arrayNumber)
+    return super.Success(res, characters)
   }
 
   async create (req, res) {
